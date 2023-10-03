fef2b63f89d25ca70c1bb23e6cbf2e56963548af
Guru18650 <guruwszystkiego@gmail.com>
2023-10-03 22:09:19 +0200

    Dodaj pliki projektów.

---
+++ ml_kolkokrzyzyk/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ml_kolkokrzyzyk
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
